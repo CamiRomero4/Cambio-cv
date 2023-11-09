c55400e6a7d8eec1753d78cd61b68cffe08aed65
Camila <camilagiaromero3@gmail.com>
2023-11-09 14:50:08 -0300

    ultimos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { InformacionPersonalComponent } from './div/informacion-personal/informacion-personal.component';
-import { PerfilProfesionalComponent } from './fieldset/perfil-profesional/perfil-profesional.component';
-import { EducacionComponent } from './fieldset/educacion/educacion.component';
-import { ProyectosComponent } from './fieldset/proyectos/proyectos.component';
-import { HabilidadesComponent } from './fieldset/habilidades/habilidades.component';
-import { ContactoComponent } from './footer/contacto/contacto.component';
+import { InformacionPersonalComponent } from './componentes/informacion-personal/informacion-personal.component';
+import { PerfilProfesionalComponent } from './componentes/perfil-profesional/perfil-profesional.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 import {HttpClientModule} from '@angular/common/http';
 
